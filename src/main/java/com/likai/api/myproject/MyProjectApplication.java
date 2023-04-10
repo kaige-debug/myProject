16adfac7403bc2c4fc72d4523d436c92b06573e5
lk <1669263585@qq.com>
2023-04-10 10:03:02 +0800

    ddd

---
+++ src/main/java/com/likai/api/myproject/MyProjectApplication.java
@@ -0,0 +1,16 @@
+package com.likai.api.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author admin
+ */
+@SpringBootApplication
+public class MyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyProjectApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/likai/api/myproject/domain/User.java
@@ -0,0 +1,16 @@
+package com.likai.api.myproject.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author admin
+ */
+@Getter
+@Setter
+public class User {
+    long userId;
+    String userName;
+
+    String password;
+}
